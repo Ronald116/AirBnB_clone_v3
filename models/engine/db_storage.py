c8471fb0825ea17ef80323c02c0f40da198972ba
ronald116 <ronaldbotchway@gmail.com>
2023-10-02 02:16:40 +0000

    pycodestyle

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -74,16 +74,16 @@ class DBStorage:
     def close(self):
         """call remove() method on the private session attribute"""
         self.__session.remove()
-    
+
     def get(self, cls, id):
         """retrieve one object"""
-        if cls in classes.values() and id and type(id) == str:
+        if cls in classes.values() and id and type(id) is str:
             obj = self.all(cls)
             for key, value in obj.items():
                 if key.split(".")[1] == id:
                     return value
         return None
-    
+
     def count(self, cls=None):
         """count the number of objects in storage"""
         data = self.all(cls)
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -55,7 +55,7 @@ class FileStorage:
                 jo = json.load(f)
             for key in jo:
                 self.__objects[key] = classes[jo[key]["__class__"]](**jo[key])
-        except:
+        except Exception as ex:
             pass
 
     def delete(self, obj=None):
@@ -68,16 +68,16 @@ class FileStorage:
     def close(self):
         """call reload() method for deserializing the JSON file to objects"""
         self.reload()
-    
+
     def get(self, cls, id):
         """retrieve one object"""
-        if cls in classes.values() and id and type(id) == str:
+        if cls in classes.values() and id and type(id) is str:
             obj = self.all(cls)
             for key, value in obj.items():
                 if key.split(".")[1] == id:
                     return value
         return None
-    
+
     def count(self, cls=None):
         """count the number of objects in storage"""
         data = self.all(cls)
