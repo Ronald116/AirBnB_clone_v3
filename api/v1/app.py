912388bc7a2d25b829c76ca54977909370266b10
ronald116 <ronaldbotchway@gmail.com>
2023-10-02 13:59:41 +0000

    correction

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,13 +3,12 @@
 from flask import Flask, make_response, jsonify
 from models import storage
 from api.v1.views import app_views
-from flask import Blueprint
 from os import getenv
 from flask_cors import CORS
 
 
 app = Flask(__name__)
-app.register_app(app_views)
+app.register_blueprint(app_views)
 CORS(app, resources={r"/api/*":{"origins": "0.0.0.0"}})
 
 
